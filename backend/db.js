229829ed4fca8115c6e8f798699490dee28b5c38
Mohd Yousuf <yousufulabrarkhan@gmail.com>
2023-04-15 17:19:44 +0530

    Data-validation

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/yousuf"
+const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
 
 const connectToMongo = async ()=>{
    await mongoose.connect(mongoURI)
