232129f041d8251eb1d71908b55b7185cb65bcdf
Mohd Yousuf <yousufulabrarkhan@gmail.com>
2023-04-15 16:43:34 +0530

    storing-in-database

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017"
+const mongoURI = "mongodb://127.0.0.1:27017/yousuf"
 
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI, ()=>{
-        console.log("connect to dp")
-    })
+const connectToMongo = async ()=>{
+   await mongoose.connect(mongoURI)
+   console.log("connected")
 }
 
 module.exports = connectToMongo;
