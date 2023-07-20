20d0280451d35c3f1be2d55c708aacde7bbd09d7
Yousuf <yousufulabrarkhan@gmail.com>
2023-07-20 17:03:27 +0530

    adding api for updating note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -40,4 +40,31 @@ router.post('/addnote', fetchuser, [
      }
 })
 
+//Route 3: //Update an existing Note using: Put "/api/notes/updatenote" login required
+router.put('/updatenote/:id', fetchuser, async (req, res)=>{
+     try {
+        const {title, description, tag} = req.body;
+
+        //create a newNote object
+        const newNote = {};
+        if(title) newNote.title = title
+        if(description) newNote.description = description
+        if(tag)  newNote.tag = tag
+
+        // Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if(!note) {return res.status(404).send("Not Found")}
+
+        //checking weather the user is a owner of that note or not
+        if(note.user.toString() !== req.user.id){
+              return res.status(401).send("Not Allowed")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true})
+        res.json({note});
+     } catch (error) {
+        console.log(error.message)
+        res.status(500).send("Internal Server Error")
+     }
+})
 module.exports = router;
